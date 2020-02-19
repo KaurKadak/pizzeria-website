e428cfd0551e75ebf15df6d7f1b414bf3f7bfdd5
Kaur Kadak <K.Kadak@student.liverpool.ac.uk>
2020-02-19 02:33:54 +0000

    Created a contact page, started working on the table booking system

---
+++ scripts/timepicker.js
@@ -0,0 +1,14 @@
+var timepicker = document.querySelector("#timepicker-input")
+var dropdownTimes = document.querySelector(".dropdown-times")
+var times = document.querySelectorAll(".time-btn")
+
+timepicker.addEventListener("click", function(){
+    dropdownTimes.classList.toggle("not-hidden")
+})
+
+for (let i = 0; i < times.length; i++){
+    times[i].addEventListener("click", function(){
+        var timeOfBooking = this.value
+        console.log(timeOfBooking)
+    })
+}
